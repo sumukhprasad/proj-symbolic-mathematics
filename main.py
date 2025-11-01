719d4257e632d365742c44e553757646872b9d3e
Sumukh Prasad <sumukhprasad@icloud.com>
2025-11-01 19:04:52 +0530

    added matplotlib graphing for points

--- main.py
+++ main.py
@@ -1,286 +1,114 @@
-import re
-import math
+import tkinter as tk
+from tkinter import scrolledtext
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
 
 
-class SymParser:
-	def __init__(self, eq):
-		eq = "".join(eq.split(" "))
-		eq = "(".join(eq.split("["))
-		eq = ")".join(eq.split("]"))
-		self.eq = eq
-		self.__fixXY()
-		self.__fixImplicitMultiplication()
-		self.__groupOperators()
-		self.__groupWords()
-		self.__fixParentheses()
+
+from solver import *
+
+
+class SymREPL(tk.Tk):
+	def __init__(self, solver):
+		super().__init__()
+		self.solver = solver
+		self.title("Symbolic Math REPL")
+		self.geometry("800x600")
+		
+		self.output = scrolledtext.ScrolledText(self, wrap=tk.WORD, state="disabled", bg="#fdf9eb", fg="#000", font=("Monaco", 12))
+		self.output.pack(fill=tk.BOTH, expand=True, padx=5, pady=5)
+		
+		self.input = tk.Entry(self, bg="#fff", fg="#000", font=("Monaco", 12))
+		self.input.pack(fill=tk.X, padx=5, pady=(0,5))
+		self.input.bind("<Return>", self.handle_input)
+		
+		self.__write("Symbolic Mathematics Project\nREPL Interface\n------\nType into the input box and press Return.\n", color="#0d7680")
+		
+	def __write(self, s, color="#355778"):
+		self.output.configure(state="normal")
+		tag_name = f"color_{color}"
+		if not tag_name in self.output.tag_names():
+			self.output.tag_configure(tag_name, foreground=color)
+		self.output.insert(tk.END, s + "\n", tag_name)
+		self.output.configure(state="disabled")
+		self.output.see(tk.END)
 	
-	
-	# tree generation from separated expression
-	# precedence: ^ over *, / over + and -
-	# expression -> term -> power -> factor
-	def parse_expression(self, tokens):
-		node = self.parse_term(tokens)
-		while tokens and tokens[0] in ['+', '-']:
-			op = tokens.pop(0)
-			right = self.parse_term(tokens)
-			node = Node(op, node, right)
-		return node
-	
-	def parse_term(self, tokens):
-		node = self.parse_power(tokens)
-		while tokens and tokens[0] in ['*', '/']:
-			op = tokens.pop(0)
-			right = self.parse_power(tokens)
-			node = Node(op, node, right)
-		return node
-	
-	def parse_power(self, tokens):
-		node = self.parse_factor(tokens)
-		while tokens and tokens[0] == '^':
-			op = tokens.pop(0)
-			right = self.parse_factor(tokens)
-			node = Node(op, node, right)
-		return node
-	
-	def parse_factor(self, tokens):
-		print("parse_factor start:", tokens)
-		token = tokens.pop(0)
-		print("popped:", token)
-		if token == '(':
-			node = self.parse_expression(tokens)
-			tokens.pop(0)  # pop ')'
-			return node
-		elif token.isalpha():
-			# could be variable or function
-			if tokens and tokens[0] == '(':
-				func = token
-				tokens.pop(0)  # remove '('
-				arg = self.parse_expression(tokens)
-				tokens.pop(0)  # remove ')'
-				return Node(func, arg)
+	def handle_input(self, event):
+		cmd = self.input.get().strip()
+		self.input.delete(0, tk.END)
+
+		if not cmd:
+			return
+		self.__write(f">>> {cmd}", color="#e6175c")
+		try:
+			self.process_command(cmd)
+		except Exception as e:
+			self.__write(f"error: {e}", color="#f00")
+			
+			
+	def process_command(self, cmd):
+		if "=" in cmd:
+			var, expr = map(str.strip, cmd.split("=", 1))
+			if expr.replace('.', '', 1).isdigit():
+				self.solver.set_var_value(var, float(expr))
+				self.__write(f"set {var} = {expr}")
 			else:
-				return Node(token)
-		elif re.match(r'^\d+(\.\d+)?$', token):
-			return Node(float(token))
-		elif token == '-':  # handle negation
-			return Node('*', Node('-1'), self.parse_factor(tokens))
+				self.solver.add_equation(var, expr)
+				self.__write(f"added equation: {var} = {expr}")
+		elif cmd.startswith("eval "):
+			var = cmd.split(" ", 1)[1]
+			val = self.solver.evaluate(var)
+			self.__write(f"{var} = {val}")
+		elif cmd.startswith("plot"):
+			parts = cmd.split()
+			if len(parts) != 6:
+				self.__write("usage: plot <evaluated_var> <varying_var> <start> <end> <step>", color="#f00")
+				return
+	
+			evar = parts[1]
+			vvar = parts[2]
+			start = float(parts[3])
+			end = float(parts[4])
+			step = float(parts[5])
+	
+			try:
+				data = self.solver.provide_graph_values(evar, vvar, (start, end), step)
+				self.__write(str(data))
+			except Exception as e:
+				self.__write(f"plot error: {e}", color="#f00")
+				return
+	
+			xs, ys = zip(*data)
+			plt.figure()
+			plt.plot(xs, ys)
+			plt.title(f"{evar} vs {vvar}")
+			plt.xlabel(vvar)
+			plt.ylabel(evar)
+			plt.grid(True)
+			self.__write(f"plotted {evar} over {vvar} in range [{start}, {end}], opening plot...", color="#0a0")
+			plt.show()
+			self.__write(f"done.", color="#0a0")
+		
+		
+		elif cmd == "vars":
+			self.__write(str(self.solver.values))
+		elif cmd == "help":
+			self.__write("commands:\n"
+						"  var = expr   define equation\n"
+						"  var = num    set value\n"
+						"  eval var     evaluate a variable\n"
+						"  plot         plot a graph\n"
+						"  vars         show all known values\n"
+						"  help         show this help\n"
+						"  clear        reset everything")
+		elif cmd == "clear":
+			self.solver = SymSolver()
+			self.__write("cleared all equations + values")
 		else:
-			raise ValueError(f"unexpected token {token}")
-	
-	
-	def parse(self):
-		exploded = [t for t in self.eq.split() if t]
-		print(exploded)
+			self.__write(f"unknown command: {cmd}")
 
-		if len(exploded)==0:
-			raise ValueError(f"no terms resolvable to tree")
-		elif len(exploded)==1:
-			print(f"length of resolved tree is 1, are you sure this is not redundant?")
-		
-		tree = self.parse_expression(exploded)
-		return tree
-	
-	
-	
-	
-	
-	# helpers
-	def __groupWords(self):
-	    words = ['arcsin', 'arccos', 'arctan', 'cosec', 'sin', 'cos', 'tan', 'sec', 'cot', 'log', 'ln', 'sqrt', 'pi', 'e', 'theta']
-	    words.sort(key=len, reverse=True)
-	    for f in words:
-	        self.eq = re.sub(rf'\b{f}\b', f' {f} ', self.eq)
-	
-	def __groupOperators(self):
-		ops = ['(', ')', '^', '/', '*', '+', '-']
-		for f in ops:
-			self.eq = re.sub(re.escape(f), f' {f} ', self.eq)
-		
-	def __fixXY(self):
-		self.eq = re.sub(r'x[y]', 'x*y', self.eq)
-		self.eq = re.sub(r'y[x]', 'y*x', self.eq)
-		self.eq = re.sub(r'([0-9a-zA-Z])x', r'\1*x', self.eq)
-		self.eq = re.sub(r'([0-9a-zA-Z])y', r'\1*y', self.eq)
-	
-	def __fixImplicitMultiplication(self):
-		self.eq = re.sub(r'(?<!sin)(?<!cos)(?<!tan)(?<!log)(?<!ln)(?<!sqrt)(?<=\w|\))\(', r'*\(', self.eq)
-		self.eq = re.sub(r'\)(?=\w|\()', r')*', self.eq)
-	
-	def __fixParentheses(self):
-		sttParCount = self.eq.count('(')
-		endParCount = self.eq.count(')')
-		if sttParCount != endParCount:
-			print(f"WARNING: parentheses mismatch ({sttParCount} '(' vs {endParCount} ')')")
-
-		
-		
-	def printEq(self):
-		print(self.eq)
-		
-
-class Node:
-	def __init__(self, value, left=None, right=None):
-		self.value = value
-		self.left = left
-		self.right = right
-
-	def __repr__(self):
-		# unary node (function probably)
-		if self.left and not self.right:
-			return f"{self.value}({self.left})"
-		# binary node
-		elif self.left and self.right:
-			return f"({self.left} {self.value} {self.right})"
-		# value
-		else:
-			return str(self.value)
-
-
-
-
-
-
-class SymSolver:
-	def __init__(self):
-		self.eqns = {}
-		self.values = {}
-		self.words = {
-			# trig
-			'sin': math.sin,
-			'cos': math.cos,
-			'tan': math.tan,
-			'arcsin': math.asin,
-			'arccos': math.acos,
-			'arctan': math.atan,
-			'sec': lambda x: 1 / math.cos(x),
-			'cosec': lambda x: 1 / math.sin(x),
-			'cot': lambda x: 1 / math.tan(x),
-			
-			# logs
-			'log': math.log10,
-			'ln': math.log,
-			'sqrt': math.sqrt,
-			
-			# constants
-			'pi': math.pi,
-			'e': math.e
-		}
-		
-		self._evaluating = set()
-		
-	def add_equation(self,var,expr):
-		parser = SymParser(expr)
-		tree = parser.parse()
-		self.eqns[var] = tree	
-		
-	def set_var_value(self, var, val):
-		self.values[var] = val
-		
-	def evaluate(self, var):
-		print("Attempting to solve for", var, "...")
-		
-		# very good luck
-		if var in self._evaluating:
-			raise ValueError(f"cannot proceed, circular dependency: {var} defined as result of function with argument {var}")
-
-		if var in self.values:
-			return self.values[var]
-		elif var in self.eqns:
-			self._evaluating.add(var)
-			res=self.__evaluateNode(self.eqns[var])
-			self._evaluating.remove(var)
-			self.values[var]=res
-			return res
-		else:
-			print(f"ERROR: no expression available for {var}")
-			
-	
-	def __evaluateNode(self, node):
-		# operands
-		if node.value in ['+', '-', '*', '/', '^']:
-			left = self.__evaluateNode(node.left)
-			right = self.__evaluateNode(node.right)
-			return self.__applyOp(node.value, left, right)
-		
-		# predefined constant
-		elif node.value in self.words and isinstance(self.words[node.value], (int, float)):
-			return self.words[node.value]
-		
-		# unary function
-		elif node.value in self.words and callable(self.words[node.value]):
-			arg = self.__evaluateNode(node.left)
-			return self.words[node.value](arg)
-		
-		# straight numbers
-		elif isinstance(node.value, (int, float)):
-			return node.value
-		
-		# previously known or computed variables
-		elif node.value in self.values:
-			return self.values[node.value]
-		
-		# evaluate other equations
-		elif node.value in self.eqns:
-			return self.evaluate(node.value)
-
-		# good luck
-		else:
-			raise ValueError(f"unknown variable {node.value}")
-	
-	
-	
-	def provide_graph_values(self, evar, vvar, vary, step):
-		inp = vary[0]
-		gvalues = []
-		while inp<vary[1]:
-			self.values[vvar]=inp
-			res=solver.evaluate(evar)
-			gvalues.append((inp,res))
-			inp+=step
-		
-		return gvalues
-			
-			
-	
-	
-	def __applyOp(self, op, left, right):
-		if op == '+': return left + right
-		if op == '-': return left - right
-		if op == '*': return left * right
-		if op == '/': return left / right
-		if op == '^': return left ** right
-
-
-
-
-
-#eq = "x^2 + y^2 - log(x) - cos[sin(y)] + (8x/6y)"
-#parser = SymParser(eq)
-#parser.printEq()
-#t = parser.parse()
-#print(t)
-
-solver = SymSolver()
-
-#solver.add_equation('x', "x^2 + y^2 - log(x) - cos[sin(y)] + (8x/6y)")
-#print(solver.evaluate('x'))
-
-
-'''
-solver.add_equation('x', "y^2+2*y*z+4*a*b*z")
-solver.add_equation('z', "(sin(y))^2")
-solver.add_equation('y', "sin(1-sqrt(2))")
-
-solver.add_equation('a', "12")
-solver.add_equation('b', "27")
-
-print("Resolved value for x:", solver.evaluate('x'))
-'''
-
-solver.add_equation('x', "y^2+2*y*z+4*a*b*z")
-solver.add_equation('z', "(arccos(y))^2")
-
-solver.add_equation('a', "12")
-solver.add_equation('b', "27")
-
-print("Resolved values for x over [-1,1] with step 0.1:", solver.provide_graph_values('x', 'y', [-1,1], 0.1))
+if __name__ == "__main__":
+	solver = SymSolver()
+	app = SymREPL(solver)
+	app.mainloop()
---
+++ solver.py
@@ -0,0 +1,290 @@
+import re
+import math
+
+
+class SymParser:
+	def __init__(self, eq):
+		eq = "".join(eq.split(" "))
+		eq = "(".join(eq.split("["))
+		eq = ")".join(eq.split("]"))
+		self.eq = eq
+		self.__fixXY()
+		self.__fixImplicitMultiplication()
+		self.__groupOperators()
+		self.__groupWords()
+		self.__fixParentheses()
+	
+	
+	# tree generation from separated expression
+	# precedence: ^ over *, / over + and -
+	# expression -> term -> power -> factor
+	def parse_expression(self, tokens):
+		node = self.parse_term(tokens)
+		while tokens and tokens[0] in ['+', '-']:
+			op = tokens.pop(0)
+			right = self.parse_term(tokens)
+			node = Node(op, node, right)
+		return node
+	
+	def parse_term(self, tokens):
+		node = self.parse_power(tokens)
+		while tokens and tokens[0] in ['*', '/']:
+			op = tokens.pop(0)
+			right = self.parse_power(tokens)
+			node = Node(op, node, right)
+		return node
+	
+	def parse_power(self, tokens):
+		node = self.parse_factor(tokens)
+		while tokens and tokens[0] == '^':
+			op = tokens.pop(0)
+			right = self.parse_factor(tokens)
+			node = Node(op, node, right)
+		return node
+	
+	def parse_factor(self, tokens):
+		print("parse_factor start:", tokens)
+		token = tokens.pop(0)
+		print("popped:", token)
+		if token == '(':
+			node = self.parse_expression(tokens)
+			tokens.pop(0)  # pop ')'
+			return node
+		elif token.isalpha():
+			# could be variable or function
+			if tokens and tokens[0] == '(':
+				func = token
+				tokens.pop(0)  # remove '('
+				arg = self.parse_expression(tokens)
+				tokens.pop(0)  # remove ')'
+				return Node(func, arg)
+			else:
+				return Node(token)
+		elif re.match(r'^\d+(\.\d+)?$', token):
+			return Node(float(token))
+		elif token == '-':  # handle negation
+			return Node('*', Node('-1'), self.parse_factor(tokens))
+		else:
+			self._evaluating = set()
+			raise ValueError(f"unexpected token {token}")
+	
+	
+	def parse(self):
+		exploded = [t for t in self.eq.split() if t]
+		print(exploded)
+
+		if len(exploded)==0:
+			self._evaluating = set()
+			raise ValueError(f"no terms resolvable to tree")
+		elif len(exploded)==1:
+			print(f"length of resolved tree is 1, are you sure this is not redundant?")
+		
+		tree = self.parse_expression(exploded)
+		return tree
+	
+	
+	
+	
+	
+	# helpers
+	def __groupWords(self):
+	    words = ['arcsin', 'arccos', 'arctan', 'cosec', 'sin', 'cos', 'tan', 'sec', 'cot', 'log', 'ln', 'sqrt', 'pi', 'e', 'theta']
+	    words.sort(key=len, reverse=True)
+	    for f in words:
+	        self.eq = re.sub(rf'\b{f}\b', f' {f} ', self.eq)
+	
+	def __groupOperators(self):
+		ops = ['(', ')', '^', '/', '*', '+', '-']
+		for f in ops:
+			self.eq = re.sub(re.escape(f), f' {f} ', self.eq)
+		
+	def __fixXY(self):
+		self.eq = re.sub(r'x[y]', 'x*y', self.eq)
+		self.eq = re.sub(r'y[x]', 'y*x', self.eq)
+		self.eq = re.sub(r'([0-9a-zA-Z])x', r'\1*x', self.eq)
+		self.eq = re.sub(r'([0-9a-zA-Z])y', r'\1*y', self.eq)
+	
+	def __fixImplicitMultiplication(self):
+		self.eq = re.sub(r'(?<!sin)(?<!cos)(?<!tan)(?<!log)(?<!ln)(?<!sqrt)(?<=\w|\))\(', r'*\(', self.eq)
+		self.eq = re.sub(r'\)(?=\w|\()', r')*', self.eq)
+	
+	def __fixParentheses(self):
+		sttParCount = self.eq.count('(')
+		endParCount = self.eq.count(')')
+		if sttParCount != endParCount:
+			print(f"WARNING: parentheses mismatch ({sttParCount} '(' vs {endParCount} ')')")
+
+		
+		
+	def printEq(self):
+		print(self.eq)
+		
+
+class Node:
+	def __init__(self, value, left=None, right=None):
+		self.value = value
+		self.left = left
+		self.right = right
+
+	def __repr__(self):
+		# unary node (function probably)
+		if self.left and not self.right:
+			return f"{self.value}({self.left})"
+		# binary node
+		elif self.left and self.right:
+			return f"({self.left} {self.value} {self.right})"
+		# value
+		else:
+			return str(self.value)
+
+
+
+
+
+
+class SymSolver:
+	def __init__(self):
+		self.eqns = {}
+		self.values = {}
+		self.words = {
+			# trig
+			'sin': math.sin,
+			'cos': math.cos,
+			'tan': math.tan,
+			'arcsin': math.asin,
+			'arccos': math.acos,
+			'arctan': math.atan,
+			'sec': lambda x: 1 / math.cos(x),
+			'cosec': lambda x: 1 / math.sin(x),
+			'cot': lambda x: 1 / math.tan(x),
+			
+			# logs
+			'log': math.log10,
+			'ln': math.log,
+			'sqrt': math.sqrt,
+			
+			# constants
+			'pi': math.pi,
+			'e': math.e
+		}
+		
+		self._evaluating = set()
+		
+	def add_equation(self,var,expr):
+		parser = SymParser(expr)
+		tree = parser.parse()
+		self.eqns[var] = tree	
+		
+	def set_var_value(self, var, val):
+		self.values[var] = val
+		
+	def evaluate(self, var):
+		print("Attempting to solve for", var, "...")
+		
+		# very good luck
+		if var in self._evaluating:
+			self._evaluating = set()
+			raise ValueError(f"cannot proceed, circular dependency: {var} defined as result of function with argument {var}")
+
+		if var in self.values:
+			return self.values[var]
+		elif var in self.eqns:
+			self._evaluating.add(var)
+			res=self.__evaluateNode(self.eqns[var])
+			self._evaluating.remove(var)
+			self.values[var]=res
+			return res
+		else:
+			print(f"ERROR: no expression available for {var}")
+			
+	
+	def __evaluateNode(self, node):
+		# operands
+		if node.value in ['+', '-', '*', '/', '^']:
+			left = self.__evaluateNode(node.left)
+			right = self.__evaluateNode(node.right)
+			return self.__applyOp(node.value, left, right)
+		
+		# predefined constant
+		elif node.value in self.words and isinstance(self.words[node.value], (int, float)):
+			return self.words[node.value]
+		
+		# unary function
+		elif node.value in self.words and callable(self.words[node.value]):
+			arg = self.__evaluateNode(node.left)
+			return self.words[node.value](arg)
+		
+		# straight numbers
+		elif isinstance(node.value, (int, float)):
+			return node.value
+		
+		# previously known or computed variables
+		elif node.value in self.values:
+			return self.values[node.value]
+		
+		# evaluate other equations
+		elif node.value in self.eqns:
+			return self.evaluate(node.value)
+
+		# good luck
+		else:
+			self._evaluating = set()
+			raise ValueError(f"unknown variable {node.value}")
+	
+	
+	
+	def provide_graph_values(self, evar, vvar, vary, step):
+		inp = vary[0]
+		gvalues = []
+		while inp<vary[1]:
+			self.values[vvar]=inp
+			res=self.evaluate(evar)
+			gvalues.append((inp,res))
+			inp+=step
+		
+		return gvalues
+			
+			
+	
+	
+	def __applyOp(self, op, left, right):
+		if op == '+': return left + right
+		if op == '-': return left - right
+		if op == '*': return left * right
+		if op == '/': return left / right
+		if op == '^': return left ** right
+
+
+
+
+
+#eq = "x^2 + y^2 - log(x) - cos[sin(y)] + (8x/6y)"
+#parser = SymParser(eq)
+#parser.printEq()
+#t = parser.parse()
+#print(t)
+#solver.add_equation('x', "x^2 + y^2 - log(x) - cos[sin(y)] + (8x/6y)")
+#print(solver.evaluate('x'))
+
+
+'''
+solver = SymSolver()
+
+solver.add_equation('x', "y^2+2*y*z+4*a*b*z")
+solver.add_equation('z', "(sin(y))^2")
+solver.add_equation('y', "sin(1-sqrt(2))")
+
+solver.add_equation('a', "12")
+solver.add_equation('b', "27")
+
+print("Resolved value for x:", solver.evaluate('x'))
+
+
+solver.add_equation('x', "y^2+2*y*z+4*a*b*z")
+solver.add_equation('z', "(arccos(y))^2")
+
+solver.add_equation('a', "12")
+solver.add_equation('b', "27")
+
+print("Resolved values for x over [-1,1] with step 0.1:", solver.provide_graph_values('x', 'y', [-1,1], 0.1))
+'''
